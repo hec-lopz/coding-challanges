fe64649cf4e91514e40da87933fc8f5608b0e6c6
Hector Lopez <hlopezcarballo@gmail.com>
2020-09-17 04:40:56 -0500

    adds challenge 4

---
+++ challenge4.js
@@ -0,0 +1,28 @@
+const { read } = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const looper = (phrase, times) => {
+    if (times === 1){
+        return phrase
+    } else {
+        return phrase += ` ${looper(phrase, times-1)}`
+    }
+}
+
+rl.question('Introduce la palabra o frase que quieres repetir: ', (phrase) => {
+    rl.question('¿Cuántas veces quieres repetirlo?: ', (repeat) => {
+        let multiplier = parseInt(repeat);
+        let final_phrase = looper(phrase, multiplier);
+        console.log(final_phrase);
+        rl.close();
+
+    })
+})
+
+rl.on("close", () => {
+    console.log('Gracias por tu visita. c:');
+    process.exit(0);
+})
